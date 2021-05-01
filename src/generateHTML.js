e607fd819f92796af951508e3da86ea9d7d40dab
Lucile <developer.lvt@gmail.com>
2021-05-01 13:42:47 -0400

    added icons role

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,44 +1,53 @@
-const Classes = require("../lib/Classes")
+const Classes = require("../lib/Classes");
 
-function generateCard(employee){
+function generateCard(employee) {
+  let newEmployee = new Classes.Employee(
+    employee.name,
+    employee.ID,
+    employee.email
+  );
 
-        let newEmployee = new Classes.Employee(employee.name, employee.ID, employee.email)
+  let newManager = new Classes.Manager(employee.number);
 
-        let newManager = new Classes.Manager(employee.number)
+  let newEngineer = new Classes.Engineer(employee.github);
 
-        let newEngineer = new Classes.Engineer(employee.github)
+  let newIntern = new Classes.Intern(employee.school);
 
-        let newIntern = new Classes.Intern(employee.school)
-
-
-        return`
+  return `
         <div class="card">
                 <div class="cardHeader">
                     ${newEmployee.getName()}
-                    ${employee.type === "Manager" ? newManager.getRole():""}
-                    ${employee.type === "Engineer" ?newEngineer.getRole() : ""}
-                    ${employee.type === "Intern" ?newIntern.getRole() :""}
+                    ${employee.type === "Manager" ? newManager.getRole() : ""}
+                    ${employee.type === "Engineer" ? newEngineer.getRole() : ""}
+                    ${employee.type === "Intern" ? newIntern.getRole() : ""}
                 </div>
                 <div class="cardBody">
                     ${newEmployee.getId()}
                     ${newEmployee.getEmail()}
-                    ${employee.type === "Manager" ? newManager.getOfficeNumber():""}
-                    ${employee.type === "Engineer" ?newEngineer.getGithub() : ""}
-                    ${employee.type === "Intern" ?newIntern.getSchool() :""}
+                    ${
+                      employee.type === "Manager"
+                        ? newManager.getOfficeNumber()
+                        : ""
+                    }
+                    ${
+                      employee.type === "Engineer"
+                        ? newEngineer.getGithub()
+                        : ""
+                    }
+                    ${employee.type === "Intern" ? newIntern.getSchool() : ""}
                 </div>
             </div>
-        `
+        `;
 }
 
-
 function generateHTML(employeesList) {
-    let cards = ""
+  let cards = "";
 
-    for(let i = 0; i < employeesList.length ; i++){
-        cards += generateCard(employeesList[i])
-    }
-    
-    return `
+  for (let i = 0; i < employeesList.length; i++) {
+    cards += generateCard(employeesList[i]);
+  }
+
+  return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -49,6 +58,12 @@ function generateHTML(employeesList) {
         <link
         href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
         rel="stylesheet"
+        />
+        <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+            integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+            crossorigin="anonymous"
         />
         <link rel="stylesheet" href="./style.css" />
         <title>My Team</title>
@@ -63,7 +78,6 @@ function generateHTML(employeesList) {
     </body>
     </html>
         `;
- 
 }
 
 module.exports = generateHTML;
